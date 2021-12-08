d74fefe0b933d5dcb5bc3e156eb49f530b42c34d
Emre Öcel <73295089+emreocell@users.noreply.github.com>
2021-12-09 00:00:19 +0300

    Add files via upload

---
+++ Lesson_2/Lists.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alpha : MonoBehaviour
+{
+    int[] list=new int[10];
+    public int[] scores;
+    int[][] matris=new int[4][4];
+    public string[] users;
+
+    string[] isim={"Emre","Muhammed","Cemil","Ismail"};
+
+    string[][] count={("Turkey","US","Germany"),("Samsun","New York","Berlin")};
+
+}
