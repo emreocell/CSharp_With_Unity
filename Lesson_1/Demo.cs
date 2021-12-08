c9fe2d7d96d5575cb7fcf276b91a6aa3f559e700
Emre Öcel <73295089+emreocell@users.noreply.github.com>
2021-12-08 23:03:39 +0300

    Update Demo.cs

--- Lesson_1/Demo.cs
+++ Lesson_1/Demo.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Demo : MonoBehaviour
 {
+    //Degisken tanimlama
+    public string ad="Emre";
+    public int yas=19;
+    private float yetenek=3.1f;
+    
+    
+    void Start(){//basladıgında
+        Debug.Log(yetenek);
+    }
+    void Update(){//her yenilenen karade
+        Debug.LogFormat("Benim adim {0} ve yasim {1}"ad,yas);//birden fazla degiskenli cikti
+    }
     void OnEnable(){//aktif oldugunda calisir
         Debug.Log("I'm enabled.");
     }
     void OnDisable(){//deaktif oldugunda calisir
         Debug.Log("I'm disabled.");
     }
+    
 }
