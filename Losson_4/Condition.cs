384e1ef9c5e67f547ee3c40117ab83feeefd37b0
Emre Öcel <73295089+emreocell@users.noreply.github.com>
2021-12-10 10:25:27 +0300

    Update Condition.cs

--- Losson_4/Condition.cs
+++ Losson_4/Condition.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Condition : MonoBehaviour
 {
     void OnDisable() {
-        int[] numbers={Random.Range(1,100),Random.Range(1,100),Random.Range(1,100)};
+        int[] numbers={Random.Range(1,100),Random.Range(1,100),Random.Range(1,100)};//random sayı atama
         string numberText;
 
 
-        for(int i=0;i<numbers.Length;i++){
-            numberText=(numbers[i]%2==0) ? "even" : "odd";
+        for(int i=0;i<numbers.Length;i++){//for dongusu ornek
+            numberText=(numbers[i]%2==0) ? "even" : "odd";//if kullanmadan degisken degistirme
             Debug.Log(numbers[i]+" is "+numberText);
         }
     }
